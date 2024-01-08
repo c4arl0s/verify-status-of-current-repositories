1b90e98febfa9bd194f41c401a3154345a514d37
c4arl0s <c.santiago.cruz@gmail.com>
2024-01-07 21:50:23 -0600

    Apply Google Shell Style

--- verifyStatus.sh
+++ verifyStatus.sh
@@ -7,18 +7,18 @@
 # sort -n -r  
 # -maxdepth 1	maximum depth equals 1 (current directory)
 
-find . -name "*" -type d -maxdepth 1 | while read DIRECTORYNAME
-do
-    if ! git --git-dir=$DIRECTORYNAME/.git --work-tree=$DIRECTORYNAME status >/dev/null 2>&1; then
-       echo "$DIRECTORYNAME is not a git repository"
-       continue
-    fi
+find . -name "*" -type d -maxdepth 1 | while read directory_name; do
+  git --git-dir=${directory_name}/.git --work-tree=${directory_name} status >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "${directory_name} is not a git repository"
+    continue
+  fi
 
-    STATUS=`git --git-dir=$DIRECTORYNAME/.git --work-tree=$DIRECTORYNAME status | grep "\buntracked\|no\b" | tail -1`
+  repository_status=$(git --git-dir=${directory_name}/.git --work-tree=${directory_name} status | grep "\buntracked\|no\b" | tail -1)
     
-    if [[ $STATUS == *"untracked"* ]]; then
-        echo "$DIRECTORYNAME repository is untracked"
-    elif [[ $STATUS == *"no"* ]]; then 
-        echo "$DIRECTORYNAME repository has changes"
-    fi
+  if [[ "${repository_status}" == *"untracked"* ]]; then
+    echo "${directory_name} repository is untracked"
+  elif [[ "${repository_status}" == *"no"* ]]; then 
+    echo "${directory_name} repository has changes"
+  fi
 done
